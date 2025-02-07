442d023b93103c47a4bcf8c9158d89cd15d2f952
DESKTOP-4PBISAO\Anudeep <anudeepramadugu@gmail.com>
2025-02-07 18:55:24 +0530

    chestview

--- MAT II Chest System/Assets/Scripts/Chest Systen/MVC/ChestView.cs
+++ MAT II Chest System/Assets/Scripts/Chest Systen/MVC/ChestView.cs
@@ -31,7 +31,6 @@ namespace ChestSystem.Chests
         {
             ////???????????
             chestTypeText.text = chestController.chestModel.chestModelSO.ChestType.ToString();
-            
         }
     }
 }
